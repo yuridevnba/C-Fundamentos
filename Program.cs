dffa9311bd2c8b61f62af29531143ea52d3d0d9b
Yuri12 <yuri.00000830267@unicap.br>
2023-07-21 10:43:47 -0300

    pilha

--- Program.cs
+++ Program.cs
@@ -1,57 +1,13 @@
 ﻿
 using C__DIO.Models;
 
-new ExemploExcecao().Metodo1();
+ Stack <int>pilha  = new Stack<int> ();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*try{
-
-string [] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt"); // cada linha vai ficar como se fosse um array.
-
-foreach(string linha in linhas){
-    System.Console.WriteLine(linha);
+pilha.Push(14); // no topo
+pilha.Push(16);
+pilha.Push(18);
+foreach(int item in pilha){
+    System.Console.WriteLine(item);
 }
 
-} 
-catch(FileNotFoundException ex){
-    System.Console.WriteLine("Ocorreu um erro na leitura do arquivo" +ex.Message);
-}
-catch(DirectoryNotFoundException ex){
-    System.Console.WriteLine("Ocorreu um erro ao encontrar o diretório , ou seja, caminho da pasta"+ex.Message);
-}
-catch(Exception ex){
-    System.Console.WriteLine("Ocorreu uma exceção genérica"+ex.Message);
-}
-
-finally{
-    System.Console.WriteLine("Chegou até aqui!");
-}*/
-
+pilha.Pop();// removendo o 18
