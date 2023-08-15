fb69dbba094cde7f3bb67d985cc03269dc38a3e3
Yuri12 <yuri.00000830267@unicap.br>
2023-08-15 11:25:32 -0300

    Removendo e Alterando elementos do dictionary

--- Program.cs
+++ Program.cs
@@ -12,3 +12,17 @@ System.Console.WriteLine($"Chave:{item.Key}, Valor:{item.Value}");
 
  }
  estados.Add("BA2","Bahia"); // a chave não pode ser repetida, mas o valor pode.
+
+ estados.Remove("BA");
+
+ estados["SP"] = "Sao Paulo - valor alterado";
+
+ string chave = "BA";
+
+ if(estados.ContainsKey(chave)){
+    System.Console.WriteLine("Valor existente"+chave);
+    System.Console.WriteLine(estados["BA"]); // obtendo o valor.
+ }
+ else{
+    System.Console.WriteLine("Valor não existente");
+ }
