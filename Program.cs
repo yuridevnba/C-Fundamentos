2065f296957f6b14342385a5ce44ec30e3e094fa
Yuri12 <yuri.00000830267@unicap.br>
2023-08-16 11:17:30 -0300

    IF Ternário

--- Program.cs
+++ Program.cs
@@ -1,35 +1,11 @@
 ﻿
 using C__DIO.Models;
 
-/*(int, string , string) tupla = (1,"YUri","Henrique"); // tupla tem 3 tipos de dados e 3 tipos de valores. apenas com uma única variável foi representado 3 valores de diferentes tipos.
-//ValueTuple <int, string> outrosexemplostupla = (1,"ana");
-//var outroexemplotuplacreate = Tuple.Create(1,"eu"); // já identifica, mas os dados não ficam genéricos.
+int numero = 10;
+bool ehPar = false;
 
 
+// IF Ternário
+ehPar = numero % 2 ==0;
 
-
-System.Console.WriteLine($"SobreNome:{tupla.Item3}");*/
-
-
-/*LeituraArquivo arquivo = new LeituraArquivo();
-
-var(sucesso,linhasArquivo, /*QuantidadeLinhas_)= arquivo.LerArquivo("Arquivos/arquivoLeitura.txt");// _ fiz o descarte, visto que essa informação não estou usando na prática.
-
-if(sucesso){
-
-   System.Console.WriteLine("Quantidade linhas do arquivo");
-
-   foreach(string linha in linhasArquivo){
-   System.Console.WriteLine(linha);
-   }
-}
-   else{
-      System.Console.WriteLine("Nao foi possível ler o arquivo");
-   }*/
-
-   Pessoa p1 = new Pessoa("Yuri","Silva");
-
-   (string nome, string sobrenome) = p1; // um densconstrutor é parecido com uma tupla, separando em variáveis separáveis.
-
-
-   System.Console.WriteLine($"{nome}{sobrenome}");
+System.Console.WriteLine($"o número{numero} é"+(ehPar?"par":"impar"));
